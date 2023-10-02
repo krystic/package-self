3988e90c2108950bdbbbae6296515fc0723505b9
Krystic <ice@krystic.com>
2023-10-02 17:21:15 +0800

    for macos

--- sync.sh
+++ sync.sh
@@ -88,7 +88,7 @@ done
 
 sed -i \
 	-e 's?include \.\./\.\./\(lang\|devel\)?include $(TOPDIR)/feeds/packages/\1?' \
-#	-e 's?2. Clash For OpenWRT?3. Applications?' \
+	-e 's?2. Clash For OpenWRT?3. Applications?' \
 	-e 's?\.\./\.\./luci.mk?$(TOPDIR)/feeds/luci/luci.mk?' \
 	*/Makefile
 
--- test.sh
+++ test.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-sed -V
+sed -i \
+	-e 's?include \.\./\.\./\(lang\|devel\)?include $(TOPDIR)/feeds/packages/\1?' \
+	-e 's?2. Clash For OpenWRT?3. Applications?' \
+	-e 's?\.\./\.\./luci.mk?$(TOPDIR)/feeds/luci/luci.mk?' \
+	*/Makefile
